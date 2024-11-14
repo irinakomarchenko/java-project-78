1cd221de1f165348b89c431ee35043dff82d7905
irinakomarchenko <irinakomarchenko@gmail.com>
2024-11-14 16:25:48 +0100

    Fix checkstyle

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -3,7 +3,7 @@ package hexlet.code.schemas;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MapSchema extends BaseSchema<Map<String, Object>> {
+public final class MapSchema extends BaseSchema<Map<String, Object>> {
     private Integer expectedSize = null;
     private Map<String, BaseSchema<String>> schemas = new HashMap<>();
 
